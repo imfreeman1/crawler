16e9f27b7058d2cb70c0461108d90ccee8e12fea
imfreeman1 <dkfrhkt@gmail.com>
2023-08-06 18:58:41 +0900

    ssr을 이용한 webCrawler 만들기 1일차

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Button from "@/components/Button";
 import baseNetwork from "@/utils/network";
+import axios from "axios";
 import React from "react";
+import puppeteer from "puppeteer";
 
 const Home = () => {
   const testNetwork = async () => {
@@ -16,6 +18,23 @@ const Home = () => {
       <Button onClick={testNetwork}>test</Button>
     </div>
   );
+};
+
+export const getServerSideProps = async ({ req, res }) => {
+  if (req.method === "GET") {
+    console.log("start");
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://www.naver.com");
+    const test = await page.evaluate(() => {});
+    console.log(test);
+    await page.close();
+    req.method = null;
+    console.log("req", req.method);
+  }
+  return {
+    props: {},
+  };
 };
 
 export default Home;
--- src/utils/network.tsx
+++ src/utils/network.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseNetwork = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://localhost:3000/",
 });
 
 export default baseNetwork;
